3e4734d1625f2308e814747c58d8a38b7a6bd995
kennethchinedu <anamsken60@gmail.com>
2025-08-20 22:41:48 +0100

    updating

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,4 +1,5 @@
-#=----- EKS
+#=---------------------------EKS
+#=---------------------------EKS
 resource "aws_iam_role" "eks-cluster-role" {
   name = "eks-cluster-role"
   assume_role_policy = jsonencode({
@@ -26,6 +27,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "cluster_node" {
   name     = "my-cluster"
   role_arn = aws_iam_role.eks-cluster-role.arn
+
   vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
@@ -34,12 +36,12 @@ resource "aws_eks_cluster" "cluster_node" {
       var.prisub2
 
     ]
-  }
+  } 
   access_config {
     authentication_mode                         = "API"
-    bootstrap_cluster_creator_admin_permissions = true
+    bootstrap_cluster_creator_admin_permissions = true 
   }
-  bootstrap_self_managed_addons = true
+  bootstrap_self_managed_addons = true 
 
   version = "1.32"
   upgrade_policy {
